eb7d2021f52af8469b36e85fd22ca395a440fc12
JackNWthepbfantic99 <staceynavin@Jacks-MacBook-Air-2.local>
2014-11-03 12:34:08 +0000

    added code

--- Lab01.java
+++ Lab01.java
@@ -12,6 +12,71 @@ import edu.fcps.karel2.Robot;
 public class Lab01 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
-     }
+     Display.openWorld("maps/school.map");    
+  Display.setSize(10, 10);
+  Display.setSpeed(3);     
+  Robot lisa = new Robot();
+  Robot ricky = new Robot(4, 5, Display.SOUTH, 0);
+  lisa.move();
+  lisa.move();
+  lisa.turnLeft();
+  lisa.move();
+  lisa.turnLeft();
+  lisa.move();
+  lisa.pickBeeper();
+  lisa.turnLeft();
+  lisa.turnLeft();
+  lisa.move();
+  lisa.turnLeft();
+  lisa.turnLeft();
+  lisa.turnLeft();
+  lisa.move();
+  lisa.turnLeft();
+  lisa.move();
+  lisa.move();
+  lisa.move();
+  lisa.turnLeft();
+  lisa.move();
+  lisa.move();
+  lisa.move();
+  lisa.turnLeft();
+  lisa.move();
+  lisa.move();
+  lisa.turnLeft();
+  lisa.turnLeft();
+  lisa.turnLeft();
+  lisa.move();
+  lisa.putBeeper();
+  ricky.pickBeeper();
+  ricky.move();
+  ricky.turnLeft();
+  ricky.move();
+  ricky.move();
+  ricky.turnLeft();
+  ricky.move();
+  ricky.move();
+  ricky.turnLeft();
+  ricky.turnLeft();
+  ricky.turnLeft();
+  ricky.move();
+  ricky.move();
+  ricky.turnLeft();
+  ricky.move();
+  ricky.turnLeft();
+  ricky.move();
+  ricky.move();
+  ricky.turnLeft();
+  ricky.turnLeft();
+  ricky.turnLeft();
+  ricky.move();
+  ricky.move();
+  ricky.turnLeft();
+  ricky.move();
+  ricky.move();
+  ricky.putBeeper();
+  ricky.turnLeft();
+  ricky.turnLeft();
+  ricky.move();
+  
+   }
 }
